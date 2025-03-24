8d46d742aedada2dd3fc9653235f02ca014944ac
Juanperias <136520331+Juanperias@users.noreply.github.com>
2025-03-24 17:35:57 -0400

    refactor: remove .unwrap and replace with ResponseError

--- src/proxy/mod.rs
+++ src/proxy/mod.rs
@@ -1,14 +1,19 @@
-use std::convert::Infallible;
 use http_body_util::Full;
 use hyper::body::Bytes;
 use hyper::{Request, Response, header};
+use thiserror::Error;
 
+#[derive(Error, Debug)]
+pub enum ResponseError {
+    #[error("Http Error {0}")]
+    HttpError(#[from] hyper::http::Error)
+}
 
-pub async fn response(_: Request<hyper::body::Incoming>) -> Result<Response<Full<Bytes>>, Infallible> {
+pub async fn response(_: Request<hyper::body::Incoming>) -> Result<Response<Full<Bytes>>, ResponseError> {
     Ok(Response::builder()
         .status(200)
         .header(header::CONTENT_TYPE, "text/html")
-        .body(Full::new(Bytes::from("<h1>Hello!<h1/>"))).unwrap())
+        .body(Full::new(Bytes::from("<h1>Hello from piu!<h1/>")))?)
 
 }
 
